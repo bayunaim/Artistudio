7f7939cc7ea1fe342ffbc8186b399a6353e487b1
bayunaim <38815740+bayunaim@users.noreply.github.com>
2025-02-11 20:31:48 +0700

    wp plugin

---
+++ wp-content/plugins/artistudio-popup/artistudio-popup.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Artistudio Popup
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     artistudio-popup
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Artistudio_Popup
+ */
+
+// Your code starts here.
